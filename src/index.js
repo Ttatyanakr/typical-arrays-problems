54e2415514a35d080fae64011eea271693ed8ee3
Ttatyanakr <ttatyanakr@yandex.ru>
2020-10-03 00:36:01 +0300

    feat: add min. max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+const emptyArray = [];
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (array && array.length) {
+        return Math.min.apply(Math, array);
+    } else {
+        return 0;
+    };
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (array && array.length) {
+        return Math.max.apply(Math, array);
+    } else {
+        return 0;
+    };
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    let sum = 0;
+    if (array && array.length) {
+        for (i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        };
+    } else {
+        return 0;
+    };
+    let average = sum / array.length;
+    return average;
+};
